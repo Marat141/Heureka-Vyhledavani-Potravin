32f0c36070b7ec060b4eb4882ab660c6a7429367
Marat141 <terest141@gmail.com>
2025-01-08 10:40:29 +0100

    Vyresit header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,12 @@
     Filtry (na boku nebo nahoře).
     Nastavení obchodů (např. modální okno)
     */
+
+   import Header from "$lib/Components/Header.svelte";
+   import SearchBar from "$lib/Components/SearchBar.svelte";
+
 </script>
+
+<Header />
+
+<SearchBar/>
