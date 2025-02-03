043cd5d23d39924bd729d9ca9a4c917cbc92e40c
Marat141 <terest141@gmail.com>
2025-02-03 09:16:43 +0100

    Vyhledavani-vyreseno, filtr na domovskou stranku-resim,

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,26 +1,38 @@
 <script>
+	import { page } from '$app/stores';
+	import { derived } from 'svelte/store';
+
 	import Header from "$lib/Components/Header.svelte";
-	import Footer	 from "$lib/Components/Footer.svelte";
+	import Footer from "$lib/Components/Footer.svelte";
 	import SearchBar from "$lib/Components/SearchBar.svelte";
 
 	let { children } = $props();
 	let isLogginIn = $state(false);
+
+	// Cesty, kde se SearchBar NEMÁ zobrazit
+	const blockedPaths = [
+		'/Settings',
+		'/NewPassword',
+		'/Login',
+		'/ForgetPassword',
+		'/Feedback',
+		'/About',
+		'/Profile', 
+		'/Register'
+	];
+
+	// Kontrolujeme, zda stránka není mezi zakázanými
+	const showSearchBar = derived(page, $page =>
+		!blockedPaths.some(path => $page.url.pathname.startsWith(path))
+	);
 </script>
 
 <Header />
-<SearchBar />
+
+{#if $showSearchBar}
+	<SearchBar />
+{/if}
 
 {@render children()}
 
-
 <Footer />
-
-
-
-
-
-
-
-<style lang="scss">
-
-</style>
