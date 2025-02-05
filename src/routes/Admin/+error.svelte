c75b9f5c422afd4182924ad2b5eef90895d25a29
Marat141 <terest141@gmail.com>
2025-02-05 15:27:42 +0100

    Reseni-Backend

---
+++ src/routes/Admin/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+	// Importuje store 'page', který obsahuje informace o aktuální stránce včetně chyb
+	import { page } from '$app/stores';
+</script>
+
+<!-- Zobrazí zprávu chyby, pokud existuje -->
+<h1>{$page.error?.message}</h1>
+
+<!-- Statická hláška pro uživatele, že nemá přístup -->
+<p>Nejste přihlášený, sem může jen administrátor</p>
+
+<!-- Odkaz pro návrat na hlavní stránku -->
+<a href="/">Zpět na hlavní stránku</a>
--- src/routes/Stores/+page.svelte
+++ src/routes/Stores/+page.svelte
@@ -5,8 +5,16 @@
 <main>
     <div class="store-container">
         <div class="img-stores">
-            <a href=""><img src="/" alt=""></a>
-
+            <a href="/Stores/Globus"><img src="/Globus-svg.svg" alt="Globus"></a>
+            <h2>Globus</h2>
+            <p>Tady je svět ještě v pořádku</p>
+            <a href="/Stores/Billa"><img src="/Billa-logo.svg" alt="Billa"></a>
+            <h2>Billa</h2>
+            <p>To dobré začíná u nás</p>
+            <a href="/Stores/Lidl"><img src="/Lidl-logo.svg" alt="Lidl"></a>
+            <h2>Lidl</h2>
+            <p>Skvělý nápad</p>
+            <p>Správná volba</p>
         </div>
     </div>
 </main>
